3bd87622969c170fa233b2955d146769394c7994
Diolindo <luis.diolindo@gmail.com>
2024-01-15 15:01:49 +0100

    Add branching

--- packages/components/locations/src/main.tsx
+++
@@ -1,8 +0,0 @@
-// Just a comment
-import type React from "react";
-
-export interface MainProps {
-  text: string;
-}
-
-export const Main: React.FC<MainProps> = ({ text }) => <main>{text}</main>;
--- packages/components/sessions/src/index.ts
+++
@@ -1,2 +0,0 @@
-// just a comment ooooooooo
-export * from "./http-server.js";
